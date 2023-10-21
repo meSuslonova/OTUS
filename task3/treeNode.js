cda7a068968262572206cf7ed3781363a96867cd
Polina Suslonova <suslonovaps@gmail.com>
2023-10-21 11:48:54 +0300

    Update treeNode.js

--- task3/treeNode.js
+++ task3/treeNode.js
@@ -1,69 +1,77 @@
 const fs = require('fs');
 const path = require('path');
+const { promisify } = require('util');
 
-function tree(dirPath, depth = Infinity, indent = '') {
-  if (depth < 0) return;
+const readdirAsync = promisify(fs.readdir);
+const statAsync = promisify(fs.stat);
 
-  fs.readdir(dirPath, (err, files) => {
-    if (err) throw err;
+async function buildTree(dirPath, depth = Infinity, indent = '') {
+const files = await readdirAsync(dirPath);
+const tree = {};
 
-    fs.stat(dirPath, (err, stats) => {
-      if (err) throw err;
+for (let i = 0; i < files.length; i++) {
+const file = files[i];
+const filePath = path.join(dirPath, file);
 
-      const isDirectory = stats.isDirectory();
+const fileStats = await statAsync(filePath);
+const isDirectory = fileStats.isDirectory();
 
-      if (!isDirectory) {
-        //console.log(indent + dirPath);
-        return;
-      }
-      //console.log(indent + dirPath);
-
-      files.forEach((file, index) => {
-        const filePath = path.join(dirPath, file);
-
-        fs.stat(filePath, (err, fileStats) => {
-
-          if (err) throw err;
-          const prefix = index === files.length - 1 ? '└──' : '├──';
-          const nestedIndent = indent + (index === files.length - 1 ? ' ' : '│');
-
-          if (fileStats.isDirectory()) {
-            tree(filePath, depth - 1, nestedIndent);
-          } else {
-            console.log(indent + prefix + ' ' + dirPath);
-            console.log(nestedIndent + prefix + ' ' + file);
-          }
-        });
-      });
-    });
-  });
+if (depth > 0 && isDirectory) {
+tree[file] = await buildTree(filePath, depth - 1, ' ' + indent);
+} else {
+tree[file] = isDirectory ? {} : null;
+}
 }
 
+return tree;
+}
+
+async function printTree(tree, indent = '') {
+const keys = Object.keys(tree);
+
+for (let i = 0; i < keys.length; i++) {
+const file = keys[i];
+const isLast = i === keys.length - 1;
+const prefix = isLast ? '└── ' : '├── ';
+const nestedIndent = indent + (isLast ? ' ' : '│ ');
+
+console.log(indent + prefix + file);
+
+if (tree[file] !== null) {
+await printTree(tree[file], nestedIndent);
+}
+}
+}
+
+async function main() {
 const args = process.argv.slice(2);
 const dirPath = args[0];
 const depthFlag = args.find((arg) => arg === '--depth' || arg === '-d');
 const depthValueIndex = args.indexOf(depthFlag) + 1;
-
 let depth = Infinity;
+
 if (depthFlag && depthValueIndex < args.length) {
-  depth = parseInt(args[depthValueIndex]);
+depth = parseInt(args[depthValueIndex]);
 }
 
-tree(dirPath, depth);
+const tree = await buildTree(dirPath, depth);
+await printTree(tree);
+}
 
-//Результат вывода:
-//node treeNode.js C:\Users\User\projects\node --depth 2
-//├── C:\Users\User\projects\node
-//│├── index.js
-//│├── C:\Users\User\projects\node\domain 
-//││├── error.js
-//│└── C:\Users\User\projects\node\cluster
-//│ └── index.js
-//│├── C:\Users\User\projects\node\domain 
-//││├── flow.js
-//│└── C:\Users\User\projects\node\domain
-//│ └── run.js
-//│├── C:\Users\User\projects\node\errors
-//││├── counter.js
-//│└── C:\Users\User\projects\node\errors
-//│ └── try-catch.js
+main().catch(error => {
+console.log(error);
+});
+
+// Результат вывода
+// node treeNode.js C:\Users\task_3 --depth 2
+// ├── cluster
+// │ └── index.js
+// ├── domain
+// │ ├── error.js
+// │ ├── flow.js
+// │ └── run.js
+// ├── errors
+// │ ├── counters.js
+// │ └── try-catch.js
+// ├── index.js
+// └── worker
