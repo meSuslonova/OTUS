96c430e3f590f76f13430aa9abf397a98fcc6cbc
Polina Suslonova <suslonovaps@gmail.com>
2023-11-01 14:09:21 +0300

    Create app.js

---
+++ task5/app.js
@@ -0,0 +1,57 @@
+import { html, css, LitElement } from 'lit';
+
+class MyTree extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+    }
+
+    ul {
+      list-style-type: none;
+      padding-left: 20px;
+    }
+  `;
+
+  static properties = {
+    treeData: { type: Object },
+  };
+
+  render() {
+    return html`
+      <ul>
+        ${this.treeData.items.map((item) => html`<my-leaf .leafData=${item}></my-leaf>`)}
+      </ul>
+    `;
+  }
+}
+customElements.define('my-tree', MyTree);
+
+class MyLeaf extends LitElement {
+  static styles = css`
+    :host {
+      display: block;
+    }
+  `;
+
+  static properties = {
+    leafData: { type: Object },
+  };
+
+  render() {
+    return html`
+      <li>${this.leafData.name}</li>
+      ${
+        this.leafData.items
+          ? html`
+              <ul>
+                ${this.leafData.items.map(
+                  (item) => html`<my-leaf .leafData=${item}></my-leaf>`
+                )}
+              </ul>
+            `
+          : ''
+      }
+    `;
+  }
+}
+customElements.define('my-leaf', MyLeaf);
